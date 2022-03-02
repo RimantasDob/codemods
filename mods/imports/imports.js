fe1f65c80d1f54eda4a3b4644c3de4c7060f9017
RimantasDob <dobrovolskis@gmail.com>
2022-03-02 12:03:17 +0200

    first commit

---
+++ mods/imports/imports.js
@@ -0,0 +1,21 @@
+import { visit } from 'recast';
+
+import { getCallExpressionVisitor } from( './src/visitors');
+import utils from './src/utils';
+
+const files = '/Users/rimantas/projects/dibs-toho/mg/index.js';
+// const files = '/Users/rimantas/projects/dibs-toho/mg/**/*.js';
+
+const main = () => {
+    const allFiles = utils.getAllFiles(files);
+
+    for (const filePath of allFiles) {
+        console.log(filePath);
+        const ast = utils.getAST(filePath);
+        const visitor = getCallExpressionVisitor();
+        visit(ast, visitor);
+        utils.updateFile(filePath, ast);
+    }
+};
+
+main();
